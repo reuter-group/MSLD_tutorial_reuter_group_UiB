67077509743504aad34b5439c4779a1e6fddce77
Parveen Gartan <63051520+pga043@users.noreply.github.com>
2024-06-11 15:19:12 +0200

    plot fix bias and pops

--- 00files/plot_ALF_flat_v3.py
+++ 00files/plot_ALF_flat_v3.py
@@ -5,78 +5,75 @@ import sys
 '''
 Written by: Parveen Gartan
 10 February 2024
+updated: 11 June 2024
+written for single site systems only
 '''
+
 end = int(sys.argv[1])
 
-l   = []
-c   = []
-s1  = []
-s2  = []
-x1  = []
-x2  = []
+nsubs=np.loadtxt('nsubs',dtype='int',ndmin=1)
+nblocks=np.sum(nsubs)
+
+fix_pop_trans = {}
+
+for block in range(nblocks):
+    fix_pop_trans[f'block{block}'] = {}
+    fix_pop_trans[f'block{block}']["fix"] = []
+    fix_pop_trans[f'block{block}']["pop"] = []
+    for var in np.arange(1, end+1, 1):
+        try:
+            alf_var = np.genfromtxt(f'variables{var}.inp', dtype=str)
+            fix_pop_trans[f'block{block}']["fix"].append(float(alf_var[block][-1]))
+            
+        except FileNotFoundError:
+            break
+        except IOError:
+            break
+
+# get single transitions at a site
+fix_pop_trans["trans"] = {}
+fix_pop_trans["trans"]["trans"] = []
 
 for var in np.arange(1, end+1, 1):
-    try:
-        alf_var = np.genfromtxt(f'../variables{var}.inp', dtype=str)
-        lam1 = float(alf_var[0][-1])  ## phi1
-        l.append(float(alf_var[1][-1]))  ## phi2
-        c.append(float(alf_var[2][-1]))  ## psi1,2
-        s1.append(float(alf_var[5][-1]))  ## omega1,2
-        s2.append(float(alf_var[6][-1]))  ## omega2,1
-        x1.append(float(alf_var[3][-1]))  ## chi1,2
-        x2.append(float(alf_var[4][-1]))  ## chi2,1
-    except FileNotFoundError:
-        break
-    except IOError:
-        break
-
-pop  = []
-trans = []
-for i in range(1, end+1):
-    try:
-        dyna = open(f'../run{i}/output_0', 'r')
-        for lines in dyna:
-            if lines.startswith('SINGLE TRANSITIONS>'):
-                line = lines.split()
-                trans.append(line[-1])
-            if lines.startswith('SINGLE POPULATION>'):
-                #print(lines.split()[-1])
-                pop.append(int(lines.split()[-1]))
-    except FileNotFoundError:
-        break
-    except IOError:
-        break
-
-xray = pop[::2]
-flip = pop[1::2]
+    dyna = open(f'run{var}/output_0', 'r')
+    block = 0
+    for lines in dyna:
+        if lines.startswith('SINGLE TRANSITIONS>'):
+            line = lines.split()
+            fix_pop_trans["trans"]['trans'].append(line[-1])
+        ## get population of each block
+        if lines.startswith('SINGLE POPULATION>'):
+            pops = int(lines.split()[-1])
+            fix_pop_trans[f'block{block}']["pop"].append(pops) 
+            block += 1
 
 # Create figure and 6 subplots
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 6))
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 6))
 
-ax[0,0].plot(l, label="Block 3")
-ax[0,0].set_title('Fixed Bias', fontsize=18)
-ax[0,0].legend(frameon=False)
+for block in range(nblocks):
+        ax[0].plot(fix_pop_trans[f'block{block}']["fix"], label=f'block{block}')
+        ax[0].set_title('Fixed Bias', fontsize=18)
+        ax[0].legend(frameon=False)
+        
+        ax[1].plot(fix_pop_trans[f'block{block}']["pop"], label=f'block{block}')
+        ax[1].set_title('Single Population', fontsize=18)
+        ax[1].legend(frameon=False)
 
-ax[0,1].plot(c)
-ax[0,1].set_title('Quadratic Bias', fontsize=18)
-#ax[0,1].legend(frameon=False)
+        ax[2].plot(fix_pop_trans["trans"]["trans"], label=f'block{block}')
+        ax[2].set_title('Transitions', fontsize=18)
+        ax[2].legend(frameon=False)
 
-ax[1,0].plot(s1, label='Block2')
-ax[1,0].plot(s2, label='Block3')
-ax[1,0].set_title('End-point Bias', fontsize=18)
-ax[1,0].legend(frameon=False)
 
-ax[1,1].plot(x1, label='Block2')
-ax[1,1].plot(x2, label='Block3')
-ax[1,1].set_title('Skew Bias', fontsize=18)
-ax[1,1].legend(frameon=False)
+ax[0].set_xlabel("ALF iteration index", fontsize=18)
+ax[1].set_xlabel("ALF iteration index", fontsize=18)
+ax[2].set_xlabel("ALF iteration index", fontsize=18)
 
-ax[1,0].set_xlabel("ALF iteration index", fontsize=18)
-ax[1,1].set_xlabel("ALF iteration index", fontsize=18)
 
+plt.show()
+quit()
 ##---------------------------
-for p in range(2):
-    for q in range(2):
+for p in range(1):
+    for q in range(3):
         # Customize the linewidth of the x and y axes
         #ax[p,q] = plt.gca()
         ax[p,q].spines['bottom'].set_linewidth(2)  # x-axis
@@ -88,6 +85,7 @@ for p in range(2):
         ax[p,q].tick_params(axis='both', which='major', labelsize=16, width=2, length=8)  # Major ticks
 
 ##--------------------------
+quit()
 
 #fig.legend(fontsize="10")
 
@@ -101,9 +99,9 @@ plt.yticks(fontsize=16)
 fig.tight_layout()
 
 # Show the plot
-plt.savefig('ALF_iterations.png', dpi=600)
-plt.savefig('ALF_iterations.svg', dpi=600)
-plt.savefig('ALF_iterations.tif', dpi=600)
+#plt.savefig('ALF_iterations.png', dpi=600)
+#plt.savefig('ALF_iterations.svg', dpi=600)
+#plt.savefig('ALF_iterations.tif', dpi=600)
 
 plt.show()
 
